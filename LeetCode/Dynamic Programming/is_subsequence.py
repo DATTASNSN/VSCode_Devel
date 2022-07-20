3481137992a03129a56189d3eb5154422693e32e
Samayamanthula Navya Sai Narayana Datta <samayamantula.saidatta@gmail.com>
2022-07-20 08:05:38 +0530

    20-07-2022

---
+++ LeetCode/Dynamic Programming/is_subsequence.py
@@ -0,0 +1,34 @@
+'''
+Link - https://leetcode.com/problems/is-subsequence/
+
+392. Is Subsequence
+
+Easy
+
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+'''
+
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if len(s) > len(t):return False
+        if len(s) == 0:return True
+        subsequence=0
+        for i in range(0,len(t)):
+            if subsequence <= len(s) -1:
+                print(s[subsequence])
+                if s[subsequence]==t[i]:
+
+                    subsequence+=1
+        return  subsequence == len(s) 
